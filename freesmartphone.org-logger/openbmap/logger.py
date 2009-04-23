67b84455365893e4a28e100c2fadee9b27ecee86
Onen <onen.om@free.fr>
2009-04-23 20:09:21 +0200

    Software version now 0.3.0 beta.

--- freesmartphone.org-logger/openbmap/logger.py
+++ freesmartphone.org-logger/openbmap/logger.py
@@ -363,7 +363,7 @@ class Config:
                                      'openBmap.conf')
     XML_LOG_VERSION = 'V2'
     # For ease of comparison in database, we use ##.##.## format for version:
-    SOFTWARE_VERSION = '00.02.01'
+    SOFTWARE_VERSION = '00.03.00beta'
     
     def __init__(self):        
         # strings which will be used in the configuration file
