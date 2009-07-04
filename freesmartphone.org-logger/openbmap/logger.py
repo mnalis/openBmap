bac19022fb6352eaaac68381034c69ee37755726
Onen <onen.om@free.fr>
2009-07-04 16:01:38 +0200

    Set logger version to unstable, 0.0.0.

--- freesmartphone.org-logger/openbmap/logger.py
+++ freesmartphone.org-logger/openbmap/logger.py
@@ -448,7 +448,7 @@ class Config:
                                      'openBmap.conf')
     XML_LOG_VERSION = 'V2'
     # For ease of comparison in database, we use ##.##.## format for version:
-    SOFTWARE_VERSION = '00.03.02'
+    SOFTWARE_VERSION = '00.00.00'
     
     def __init__(self):        
         # strings which will be used in the configuration file
