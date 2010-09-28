4602196ea099830682ee0653aedfb16d324b1471
Onen <onen.om@free.fr>
2010-09-28 19:52:16 +0200

    Switched to python new style class.

--- freesmartphone.org-logger/openbmap/plugins/obmplugin.py
+++ freesmartphone.org-logger/openbmap/plugins/obmplugin.py
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-class ObmPlugin:
+class ObmPlugin(object):
     """Abstract base class for logging plugins.
 
     Every plugin should extend this abstract class, which contains expected content for a plugin.
